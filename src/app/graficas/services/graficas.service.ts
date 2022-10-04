42ff65a0f3122827f93def415b9812de75cbef8c
Martín <magadiflo@gmail.com>
2022-10-03 23:14:24 -0500

    Gráfica de Dona mediante petición Http

--- src/app/graficas/services/graficas.service.ts
+++ src/app/graficas/services/graficas.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface RedesSociales {
+  facebook: number;
+  youtube: number;
+  whatsapp: number;
+  "facebook-messenger": number;
+  instagram: number;
+}
+
+const URL: string = 'http://localhost:3000/grafica';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraficasService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsuariosRedesSociales(): Observable<RedesSociales> {
+    return this.http.get<RedesSociales>(URL);
+  }
+
 }
