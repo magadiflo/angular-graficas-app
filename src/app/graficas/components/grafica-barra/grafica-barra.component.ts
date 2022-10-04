1e801d6222ea3f4315f3b04631c37bf9fd703858
Martín <magadiflo@gmail.com>
2022-10-03 22:30:45 -0500

    Añadir flexibilidad a nuestro componente personalizado

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Label } from 'ng2-charts';
 import { ChartType, ChartOptions, ChartDataSets } from 'chart.js';
@@ -14,22 +14,20 @@ import { ChartType, ChartOptions, ChartDataSets } from 'chart.js';
 })
 export class GraficaBarraComponent implements OnInit {
 
+  @Input() horizontal: boolean = false;
+  @Input() barChartLabels: Label[] = [];
+  @Input() barChartData: ChartDataSets[] = [];
+
   public barChartOptions: ChartOptions = {
     responsive: true,
   };
-  public barChartLabels: Label[] = ['2020', '2021', '2022', '2023', '2024', '2025', '2026'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
-
-  public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#46E374', hoverBackgroundColor: 'blue' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#E34674', hoverBackgroundColor: '#7446E3' },
-    { data: [8, 38, 70, 59, 66, 80, 100], label: 'Series C', backgroundColor: '#7446E3', hoverBackgroundColor: 'red' },
-  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.barChartType = this.horizontal ? 'horizontalBar' : 'bar';
   }
 
 }
